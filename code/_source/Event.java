e4e7c2905fd6956af0bfda7827620b9002abc636
noracato <ncschinkel@gmail.com>
2018-10-04 14:02:32 +0200

    makefile fully operational :)

--- code/_source/Event.java
+++ code/_source/Event.java
@@ -9,34 +9,3 @@ public abstract class Event{
 		return timeEvent;
 	}
 }
-class Arrival extends Event{
-	public Arrival(double timeEvent, Tram tram){
-		super(timeEvent, tram);
-	}
-
-}
-
-class Departure extends Event{
-	int tramStopID;
-	public Departure(double timeEvent, Tram tram, int tramStopID){
-		super(timeEvent, tram);
-		this.tramStopID = tramStopID;
-	}
-}
-
-class Tram{
-	double[] scheduledArr = new double[16]; //assuming one trip is a round-trip starting from Uithof
-	int numPassengers;
-	int location = 0;
-	public Tram(double[] scheduledArr, int numPassengers){
-		this.scheduledArr = scheduledArr;
-		this.numPassengers = numPassengers;
-	}
-	public void addPassengers(int numPassengers)
-	{
-		this.numPassengers += numPassengers;
-	}
-	public int getNumPassengers(){
-		return this.numPassengers;
-	}
-}
---
+++ code/_source/Tram.java
@@ -0,0 +1,16 @@
+public class Tram {
+	double[] scheduledArr = new double[16]; //assuming one trip is a round-trip starting from Uithof
+	int numPassengers;
+	int location = 0;
+	public Tram(double[] scheduledArr, int numPassengers){
+		this.scheduledArr = scheduledArr;
+		this.numPassengers = numPassengers;
+	}
+	public void addPassengers(int numPassengers)
+	{
+		this.numPassengers += numPassengers;
+	}
+	public int getNumPassengers(){
+		return this.numPassengers;
+	}
+}
